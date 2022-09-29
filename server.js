2adbb8a9dc5bfe24ffede50fc1b6d6823e4b29b1
Lucas-Melo0 <lucassss8v@gmail.com>
2022-09-29 08:24:27 -0300

    feat: Establish connection with Postgre

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import pgk from "pg";
 
+const { Pool } = pgk;
 const server = express();
+dotenv.config();
+server.use(express.json());
+
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 server.listen(4000, () => {
   console.log("listen on 4000");
